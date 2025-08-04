7ad934f5e88c8f9f99295eab2f7ff42d5ad9036d
ASIF INAMDAR <inamdarasif678@gmail.com>
2025-08-04 07:12:36 +0000

    Measures loading performance. To provide a good user experience, LCP sho

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,24 +4,8 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { Toaster } from "@/components/ui/toaster";
 import { type ThemeProviderProps } from 'next-themes/dist/types';
-import { useState, useEffect } from 'react';
-import { Loader } from '@/components/loader';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 800); 
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
     <NextThemesProvider
       attribute="class"
