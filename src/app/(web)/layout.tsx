8d3d725caa1570b6f69dd527dfec41667a701748
ASIF INAMDAR <inamdarasif678@gmail.com>
2025-09-09 14:13:25 +0000

    Don't add new things only make sure existing things working perfectly

--- src/app/(web)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Providers } from '@/app/providers';
-
-export default function WebLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <Providers>{children}</Providers>;
-}
