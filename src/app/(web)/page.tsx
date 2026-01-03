27c7dea3c63aeaeef9678f58ebaef1f4fe6ba0bc
ASIF INAMDAR <inamdarasif678@gmail.com>
2026-01-03 11:38:07 +0000

    Can we put that Cryptocurrencies We Accept section below after our plans

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
       <main className="flex-1">
         <HeroSection />
         <FeaturesSection />
-        <CryptoCarouselSection />
         <PricingSection />
+        <CryptoCarouselSection />
         <TestimonialSection />
         <FaqSection />
         <CtaSection />
